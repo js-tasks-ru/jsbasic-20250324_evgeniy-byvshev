eccd3d59189620197b73ff65a8e93c4f0c5a1644
byvshev <byvschev2013@yandex.ru>
2025-03-29 21:54:23 +0700

    Сделана проверка на спам

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,6 @@
 function checkSpam(str) {
-  // ваш код...
+  return str.toLowerCase().replaceAll(" ", "") === "1xbetnow" ||
+    str.toLowerCase().replaceAll(" ", "") === "freexxxxx"
+    ? true
+    : false;
 }
