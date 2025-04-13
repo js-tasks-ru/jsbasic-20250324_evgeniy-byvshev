33e7adb15e795c1bf245760bbab69d5338ad9831
byvshev <byvschev2013@yandex.ru>
2025-04-13 22:25:22 +0700

    Выполнены задания пятого занятия

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,10 @@
 function toggleText() {
-  // ваш код...
+  const button = document.querySelector('.toggle-text-button');
+  const textElement = document.getElementById('text');
+
+  if (button && textElement) {
+    button.addEventListener('click', () => {
+      textElement.hidden = !textElement.hidden;
+    });
+  }
 }
